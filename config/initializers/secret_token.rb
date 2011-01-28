40288cbd628cd5cad4799d4c17d904b658fb3392
Paolo Perego <thesp0nge@gmail.com>
2011-01-28 17:10:44 +0100

    First import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackforschool::Application.config.secret_token = 'e5a2f5a740bbf39381d22e04ca492c81a787ffbcc02c8f8358cf1fe9f022eb8159d87ff213d953fed8eba71a2ba2ecaf5926dee64f644b2200449709c63b6264'
